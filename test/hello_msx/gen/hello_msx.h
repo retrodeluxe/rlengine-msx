1a0d3e9982b28374426c1a0a912029000e8cd11c
geijoenr <enric.geijo@gmail.com>
2016-09-20 07:38:41 +0000

    remove wrongly commited generated files

--- test/hello_msx/gen/hello_msx.h
+++
@@ -1,3 +0,0 @@
-#include "/Users/geijoenr/Projects/MSX/rlengine-msx1/test/hello_msx/gen/walker.h"
-#include "/Users/geijoenr/Projects/MSX/rlengine-msx1/test/hello_msx/gen/test_map.h"
-#include "/Users/geijoenr/Projects/MSX/rlengine-msx1/test/hello_msx/gen/test_tiles.h"
