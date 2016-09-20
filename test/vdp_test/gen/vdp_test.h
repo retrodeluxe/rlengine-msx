127a46adbbcdd74acb6f0a64983ef67892f9dcd5
geijoenr <enric.geijo@gmail.com>
2016-09-20 07:39:42 +0000

    remove wrongly commited gen files

--- test/vdp_test/gen/vdp_test.h
+++
@@ -1,3 +0,0 @@
-#include "/Users/geijoenr/Projects/MSX/rlengine-msx1/test/vdp_test/gen/walker.h"
-#include "/Users/geijoenr/Projects/MSX/rlengine-msx1/test/vdp_test/gen/test_map.h"
-#include "/Users/geijoenr/Projects/MSX/rlengine-msx1/test/vdp_test/gen/test_tiles.h"
