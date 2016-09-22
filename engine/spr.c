a203ac5cd8699f9319e9360a125f7bcf5f9faaaa
geijoenr <enric.geijo@gmail.com>
2016-09-22 08:22:09 +0000

    sprite: refactor sprite handling

--- engine/spr.c
+++ engine/spr.c
@@ -22,98 +22,106 @@
 #include "sprite.h"
 #include "log.h"
 
-static byte spr_attr_valloc[vdp_hw_max_sprites];
-static byte spr_patt_valloc[vdp_hw_max_patterns];
+// sprite attribute allocation table
+byte spr_attr_valloc[vdp_hw_max_sprites];
 
-const struct spr_delta_pos spr_stick2coords[] = {
-	{0, -2}, {1, -1}, {2, 0}, {1, 1},
-	{0, 2}, {-1, 1}, {-2, 0}, {-1, -1}
-};
+// sprite pattern allocation table
+byte spr_patt_valloc[vdp_hw_max_patterns];
 
+/**
+ * spr_init: initialize vdp sprites and allocation tables
+ */
 void spr_init(char spritesize, char zoom)
 {
 	vdp_init_hw_sprites(spritesize, zoom);
-	// FIXME : I think empty patterns should be off screen.
-	vdp_memset(vdp_base_spatr_grp1,
-		   sizeof(struct vdp_hw_sprite) * vdp_hw_max_sprites, 0);
+	// set all atributes out of screen
+	vdp_memset(vdp_base_spatr_grp1, sizeof(struct vdp_hw_sprite) * vdp_hw_max_sprites, 212);
 	sys_memset(spr_attr_valloc, 1, vdp_hw_max_sprites);
 	sys_memset(spr_patt_valloc, 1, vdp_hw_max_patterns);
 }
 
-static byte array__find_gap(byte * array, byte size)
+/**
+ * spr_valloc_pattern_set:
+ *		finds a gap to allocate a pattern set
+ */
+byte spr_valloc_pattern_set(struct spr_sprite_pattern_set *ps)
 {
-	byte i, nfree;
-
-	nfree = 0;
-	for (i = 0; i < vdp_hw_max_patterns - 1; i++) {
-		nfree = nfree * array[i] + array[i];
-		if (nfree == size)
-			return i - size + 1;
-	}
-	return 1;
-}
-
-byte spr_valloc(struct spr_sprite_def * sp)
-{
-	byte ipat, iatt;
 	uint npat;
+	byte i, idx, f = 0;
 
-	npat = sp->n_planes * sp->n_dirs * sp->n_anim_steps * sp->size;
-	ipat = array__find_gap(spr_patt_valloc, npat);
-	iatt = array__find_gap(spr_attr_valloc, sp->n_planes);
-	if (ipat == 1 || iatt == 1)
-		return 1;
-	sys_memset(&spr_attr_valloc[iatt], 0, sp->n_planes);
-	sys_memset(&spr_patt_valloc[ipat], 0, npat);
-	sp->base_hw_patt = ipat;
-	sp->base_hw_attr = iatt;
-	vdp_copy_to_vram(sp->patterns, vdp_base_sppat_grp1 + ipat * 8,
-			 npat * 8);
-	return 0;
+	npat = ps->n_planes * ps->n_dirs * ps->n_anim_steps * ps->size;
+
+	for (i = 0; i < vdp_hw_max_patterns - 1; i++) {
+		f = f * spr_patt_valloc[i] + spr_patt_valloc[i];
+		if (f == npat) {
+			idx = i - npat + 1;
+			sys_memset(&spr_patt_valloc[idx], 0, npat);
+			vdp_copy_to_vram(ps->patterns, vdp_base_sppat_grp1 + idx * 8, npat * 8);
+			ps->pidx = idx;
+			return true;
+		}
+	}
+	return false;
 }
 
-void spr_vfree(struct spr_sprite_def *sp)
+void spr_vfree_pattern_set(struct spr_sprite_pattern_set *ps)
 {
 	byte npat;
-
-	npat = sp->n_planes * sp->n_dirs * sp->n_anim_steps * sp->size;
-	sys_memset(&spr_attr_valloc[sp->base_hw_attr], 1, sp->n_planes);
-	sys_memset(&spr_patt_valloc[sp->base_hw_patt], 1, npat);
+	npat = ps->n_planes * ps->n_dirs * ps->n_anim_steps * ps->size;
+	sys_memset(&spr_patt_valloc[ps->pidx], 1, npat);	
 }
 
 static void spr_calc_patterns(struct spr_sprite_def *sp)
 {
-	byte i, ppd, b1, b2;
+	byte i, base, frame;
+	
+	struct spr_sprite_pattern_set *ps = sp->pattern_set;
 
-	ppd = sp->n_planes * sp->n_anim_steps;
-	b1 = sp->cur_dir * ppd;
-	b2 = sp->cur_anim_step * sp->n_planes;
-	for (i = 0; i < sp->n_planes; i++)
-		(sp->planes[i]).pattern = (b1 + b2 + i) * sp->size;
+	base = sp->cur_dir * ps->n_anim_steps * (ps->size * ps->n_planes);
+	frame = sp->cur_anim_step * (ps->size * ps->n_planes);
+	for (i = 0; i < ps->n_planes; i++)
+		(sp->planes[i]).pattern = ps->pidx + base + frame + i * ps->size ;
 }
 
-void spr_show(struct spr_sprite_def *sp)
+void spr_update(struct spr_sprite_def *sp)
 {
 	byte i;
-
 	spr_calc_patterns(sp);
-	for (i = 0; i < sp->n_planes; i++)
-		vdp_set_hw_sprite_di((byte *) & (sp->planes[i]),
-				     sp->base_hw_attr + i);
+	for (i = 0; i < sp->pattern_set->n_planes; i++)
+		vdp_set_hw_sprite(sp->aidx + i, &sp->planes[i]);
+}
 
+/**
+ * spr_show: finds a gap to allocate the attribute set
+ */
+byte spr_show(struct spr_sprite_def *sp)
+{
+	byte i, idx, n, f = 0;
+	n = sp->pattern_set->n_planes;
+	for (i = 0; i < vdp_hw_max_sprites - 1; i++) {
+		f = f * spr_attr_valloc[i] + spr_attr_valloc[i];
+		if (f == n) {
+			idx = i - n + 1;
+			sys_memset(&spr_attr_valloc[idx], 0, n);
+			sp->aidx = idx;
+			spr_update(sp);
+			return true;
+		}
+	}
+	return false;
 }
 
 void spr_hide(struct spr_sprite_def *sp)
 {
 	vdp_memset(vdp_base_spatr_grp1 +
-		   sp->base_hw_attr * sizeof(struct vdp_hw_sprite),
-		   sizeof(struct vdp_hw_sprite) * sp->n_planes, 0);
+		   sp->aidx * sizeof(struct vdp_hw_sprite),
+		   sizeof(struct vdp_hw_sprite) * sp->pattern_set->n_planes, 0);
 }
 
 void spr_set_pos(struct spr_sprite_def *sp, byte xp, byte yp)
 {
 	byte i;
-	for (i = 0; i < sp->n_planes; i++) {
+	for (i = 0; i < sp->pattern_set->n_planes; i++) {
 		(sp->planes[i]).x = xp;
 		(sp->planes[i]).y = yp;
 	}
@@ -122,32 +130,39 @@ void spr_set_pos(struct spr_sprite_def *sp, byte xp, byte yp)
 void spr_set_plane_colors(struct spr_sprite_def *sp, byte * colors)
 {
 	byte i;
-	for (i = 0; i < sp->n_planes; i++)
+	for (i = 0; i < sp->pattern_set->n_planes; i++)
 		(sp->planes[i]).color = colors[i];
 }
 
-void spr_move(struct spr_sprite_def *sp, byte dir, byte steps, char collision)
+/**
+ * How does it work when there are only 2 directions....
+ *
+ */
+void spr_animate(struct spr_sprite_def *sp, signed char dx, signed char dy, char collision)
 {
 	byte old_dir, x, y;
-	char dx, dy;
 
 	old_dir = sp->cur_dir;
 
-	dx = spr_stick2coords[dir - 1].dx * steps;
-	dy = spr_stick2coords[dir - 1].dy * steps;
-
-	if (sp->n_dirs < 5) {
-		/* hack because bad ordering of sprite data... FIXME */
+	if (sp->pattern_set->n_dirs < 3) {
+		// handle 2 directions
 		if (dx > 0)
 			sp->cur_dir = 1;
 		if (dx < 0)
-			sp->cur_dir = 2;
+			sp->cur_dir = 0;
+	} else if (sp->pattern_set->n_dirs < 5) {
+		// handle 4 directions
+		if (dx > 0)
+			sp->cur_dir = 3;
+		if (dx < 0)
+			sp->cur_dir = 1;
 		if (dy > 0)
 			sp->cur_dir = 0;
 		if (dy < 0)
-			sp->cur_dir = 3;
+			sp->cur_dir = 2;
 	} else {
-		sp->cur_dir = dir - 1;
+		// handle 8 directions
+		//sp->cur_dir = dir - 1;
 	}
 
 	if (old_dir == sp->cur_dir) {
@@ -166,17 +181,13 @@ void spr_move(struct spr_sprite_def *sp, byte dir, byte steps, char collision)
 		sp->cur_anim_step = 0;
 	}
 
-	if (sp->cur_anim_step > sp->n_anim_steps - 1)
+	if (sp->cur_anim_step > sp->pattern_set->n_anim_steps - 1)
 		sp->cur_anim_step = 0;
 
 	x = (sp->planes[0]).x + dx;
 	y = (sp->planes[0]).y + dy;
-	if (!collision)
+	if (!collision) {
 		spr_set_pos(sp, x, y);
-	spr_show(sp);
-}
-
-static void spr_handler_5th_sprite_interleaving()
-{
-	// no idea how to do this...
+	}
+	spr_update(sp);
 }
