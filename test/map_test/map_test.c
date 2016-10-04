fc27828cdbac773c61d153bade9b594abd8bc669
geijoenr <geijoenr@gmail.com>
2016-10-04 07:37:42 +0200

    map_test: update with map decompression

--- test/map_test/map_test.c
+++ test/map_test/map_test.c
@@ -12,6 +12,8 @@
 #include "sprite.h"
 #include "wq.h"
 #include "tile.h"
+#include "log.h"
+#include "map.h"
 #include "gen/map_test.h"
 #include <stdlib.h>
 
@@ -19,12 +21,14 @@ struct tile_set logo;
 struct tile_set kv;
 
 byte fb[768];
+byte map_buf[8000];
 
 void main()
 {
 	byte i,x,y, d;
 	byte *ptr;
 	byte *src;
+	unsigned int ct;
 
 	vdp_set_mode(vdp_grp2);
 	vdp_set_color(vdp_white, vdp_black);
@@ -36,16 +40,16 @@ void main()
 	 * load a tile set and generate a tile map manually
 	 */
 
-	//INIT_TILE_SET(logo, retro_logo);
-	//tile_set_to_vram(&logo, 0);
+	INIT_TILE_SET(logo, retro_logo);
+	tile_set_to_vram(&logo, 0);
 
-	//i = 0;
-	//for (y=0; y < retro_logo_tile_h; y++)
-//		for (x=0; x < retro_logo_tile_w; x++)
-//			vdp_poke(vdp_base_names_grp1 + 11 + 9 * 32 + x + y * 32, i++);
+	i = 0;
+	for (y=0; y < retro_logo_tile_h; y++)
+		for (x=0; x < retro_logo_tile_w; x++)
+			vdp_poke(vdp_base_names_grp1 + 11 + 9 * 32 + x + y * 32, i++);
 
-//	do {
-//	} while (sys_get_key(8) & 1);
+	do {
+	} while (sys_get_key(8) & 1);
  
 	/*
 	 * load a pre-processed tile map
@@ -54,6 +58,9 @@ void main()
 	INIT_TILE_SET(kv, tiles2);
 	// need an offset of one
 	tile_set_to_vram(&kv, 1);
+
+	// inflate map into a buffer, at once
+ 	map_inflate(map_cmpr_dict, map, map_buf, 2000, map_w);
 
 	x =0; y =0;
     do {
@@ -68,12 +75,14 @@ void main()
     		x--;
 
 		ptr = fb;
-		src = map + x + y * map_w;
+		src = map_buf + x + y * map_w;
 		for (i = 0; i <= 23; i++) {
 			sys_memcpy(ptr, src, 32);
 			ptr += gfx_screen_tile_w;
 			src += map_w;
 		}
+
+		//map_inflate_screen(map_cmpr_dict, map, fb, map_w, x, y);
 		vdp_fastcopy_nametable(fb);
 
 	} while(1); 
