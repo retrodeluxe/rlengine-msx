3a2a78fbd1adef63c7d9d2f3c929802a452948af
geijoenr <enric.geijo@gmail.com>
2017-02-26 20:40:14 +0100

    map: add macro for map_inflate_screen

--- engine/include/map.h
+++ engine/include/map.h
@@ -2,8 +2,11 @@
 #define _MSX_H_MAP
 
 
+
+#define map_inflate_screen(MAP, BUF, X, Y)	__map_inflate_screen(MAP ## _cmpr_dict, MAP, BUF, MAP ## _w, X, Y);
+
 void map_inflate(const uint8_t * dict, const uint8_t * in, uint8_t * out, uint16_t data_size,
 		 uint8_t w);
-void map_inflate_screen(const uint8_t * dict, const uint8_t * in, uint8_t * out, uint8_t w, uint8_t vpx, uint8_t vpy);
+void __map_inflate_screen(const uint8_t * dict, const uint8_t * in, uint8_t * out, uint8_t w, uint8_t vpx, uint8_t vpy);
 
 #endif
--- engine/map.c
+++ engine/map.c
@@ -55,7 +55,7 @@ void map_inflate(const uint8_t * dict, const uint8_t * in, uint8_t * out, uint16
  * Inflate only a 32x24 window of the map to a buffer, useful if the map is too big
  * to be decompressed entirely.
  */
-void map_inflate_screen(const uint8_t * dict, const uint8_t * in, uint8_t * out, uint8_t w, uint8_t vpx, uint8_t vpy)
+void __map_inflate_screen(const uint8_t * dict, const uint8_t * in, uint8_t * out, uint8_t w, uint8_t vpx, uint8_t vpy)
 {
 	unsigned int idx;
 	uint8_t *base = in + vpx / 2 + (vpy / 2) * w / 2;
