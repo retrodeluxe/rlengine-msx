ce4fde8cd5cce9ecf51a4575f8d87dad45966e41
geijoenr <enric.geijo@gmail.com>
2019-11-06 21:34:17 +0100

    build: compile music (pt3) resources

--- build/tools.mk
+++ build/tools.mk
@@ -5,10 +5,11 @@ TOOLS_ROOT := $(TOP)/tools
 export HEX2BIN := $(BUILD_OUT_TOOLS)/hex2bin
 export TGA2H := $(BUILD_OUT_TOOLS)/tga2header
 export TMU2H := $(BUILD_OUT_TOOLS)/tmu2header
+export XXD   := $(BUILD_OUT_TOOLS)/xxd
 
-export BUILT_TOOLS := $(HEX2BIN) $(TGA2H) $(TMU2H)
+export BUILT_TOOLS := $(HEX2BIN) $(TGA2H) $(TMU2H) $(XXD)
 
-all: $(TGA2H) $(HEX2BIN) $(TMU2H)
+all: $(TGA2H) $(HEX2BIN) $(TMU2H) $(XXD)
 
 $(TGA2H): $(TOOLS_ROOT)/tga2header.c
 	$(hide) $(HOSTCC) $^ -o $@
@@ -17,4 +18,7 @@ $(HEX2BIN): $(TOOLS_ROOT)/hex2bin.c
 	$(hide) $(HOSTCC) $^ -o $@
 
 $(TMU2H): $(TOOLS_ROOT)/tmu2header.c
+	$(hide) $(HOSTCC) $^ -o $@
+
+$(XXD): $(TOOLS_ROOT)/xxd.c
 	$(hide) $(HOSTCC) $^ -o $@
