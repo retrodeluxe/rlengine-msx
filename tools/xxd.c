a457c473e7924ed67cb006cd9e210dc1fba08b41
geijoenr <enric.geijo@gmail.com>
2020-05-15 21:07:33 +0200

    tools: xxd sizes should be const

--- tools/xxd.c
+++ tools/xxd.c
@@ -763,7 +763,7 @@ main(int argc, char *argv[])
 
       if (fp != stdin)
 	{
-	  if (fprintf(fpo, "unsigned int %s", isdigit((int)argv[1][0]) ? "__" : "") < 0)
+	  if (fprintf(fpo, "const unsigned int %s", isdigit((int)argv[1][0]) ? "__" : "") < 0)
 	    die(3);
 	  for (e = 0; (c = argv[1][e]) != 0; e++)
         if (putc(isalnum(c) ? CONDITIONAL_CAPITALIZE(c) : '_', fpo) == EOF)
