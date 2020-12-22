598e18766fc819a99b3d3d94f4a6499094f55896
geijoenr <geijoenr@gmail.com>
2020-12-22 21:30:49 +0100

    test: update sprite tests

--- test/spr_test/spr_test.c
+++ test/spr_test/spr_test.c
@@ -19,14 +19,16 @@
  enum spr_patterns_t {
  	PATRN_BEE,
  	PATRN_RAT,
- 	PATRN_EGG,
  	PATRN_MONK,
+  PATRN_BAT,
+  PATRN_DEATH,
   PATRN_KVALLEY
  };
 
-SpriteDef eggspr;
 SpriteDef monk;
 SpriteDef kvalley;
+SpriteDef bat;
+SpriteDef death_spr;
 SpriteDef bee[10];
 SpriteDef rats[10];
 
@@ -39,46 +41,82 @@ const uint8_t control_colors [1] = {6};
 int16_t xpos[10], ypos[10];
 int16_t xpos2[10], ypos2[10];
 
+const uint8_t kv_states[] = {4,4};
+const uint8_t monk_states[] = {3,3};
+const uint8_t two_states[] = {2,2};
+const uint8_t two_states2[] = {3,3};
+const uint8_t four_states[] = {3,3,3,3};
+
+void test_16x16_3layers();
+void test_16x32_2layers();
+void test_32x16_1layers();
+void test_32x32_1layer();
+void test_5th_sprite();
+
 void main()
 {
 	unsigned int count = 0;
 	uint8_t i;
-	uint8_t two_states[] = {2,2};
-	uint8_t four_states[] = {3,3,3,3};
-	uint8_t monk_states[] = {3,3};
-  uint8_t kv_states[] = {4,4};
+
 
 	vdp_set_mode(MODE_GRP1);
 	vdp_set_color(COLOR_WHITE, COLOR_BLUE);
 	vdp_clear(0);
 
-	spr_init();
-	/**
-	 * Composite sprites
-	 */
-	SPR_DEFINE_PATTERN_SET(PATRN_KVALLEY, SPR_SIZE_16x16, 3, 2, kv_states, valley2);
-	spr_valloc_pattern_set(PATRN_KVALLEY);
-
-	xpos[0] = 200; ypos [0] = 100;
-	spr_init_sprite(&kvalley, PATRN_KVALLEY);
-	spr_set_pos(&kvalley, xpos[0], ypos[0]);
-	spr_show(&kvalley);
-
-	sys_irq_init();
+  test_16x16_3layers();
+  test_16x32_2layers();
+  test_32x16_1layers();
+  test_32x32_1layer();
+  test_5th_sprite();
 
 	do {
-		sys_wait_vsync();
-		spr_refresh();
-		sys_sleep_ms(10);
-		spr_animate(&kvalley,-1,0);
-		if (xpos[0]-- < -16) xpos[0] = 255;
-		spr_set_pos(&kvalley, xpos[0], ypos[0]);
-		spr_update(&kvalley);
 	} while (sys_get_key(8) & 1);
+}
+
+/*
+ *
+ */
+void test_16x16_3layers() {
+  int8_t dir;
+  uint16_t ctr;
 
   spr_init();
+  SPR_DEFINE_PATTERN_SET(PATRN_KVALLEY, SPR_SIZE_16x16, 3, 2, kv_states, valley2);
+  spr_valloc_pattern_set(PATRN_KVALLEY);
 
-  SPR_DEFINE_PATTERN_SET(PATRN_MONK, SPR_SIZE_16x32, 1, 2, monk_states, monk1);
+  xpos[0] = 200; ypos [0] = 100;
+  spr_init_sprite(&kvalley, PATRN_KVALLEY);
+  spr_set_pos(&kvalley, xpos[0], ypos[0]);
+  spr_show(&kvalley);
+
+  sys_irq_init();
+
+  dir = -1; ctr = 0;
+  do {
+    sys_wait_vsync();
+    spr_refresh();
+    spr_animate(&kvalley,dir,0);
+    xpos[0] += dir; ctr++;
+    if (xpos[0] < -16) xpos[0] = 255;
+    if (xpos[0] > 255) xpos[0] = -16;
+    if (ctr > 300) {
+      ctr = 0;
+      dir *= -1;
+    }
+    spr_set_pos(&kvalley, xpos[0], ypos[0]);
+    spr_update(&kvalley);
+  } while (sys_get_key(8) & 1);
+}
+
+/**
+ *
+ */
+void test_16x32_2layers() {
+  int8_t dir;
+  uint16_t ctr;
+
+  spr_init();
+  SPR_DEFINE_PATTERN_SET(PATRN_MONK, SPR_SIZE_16x32, 2, 2, monk_states, monk1);
   spr_valloc_pattern_set(PATRN_MONK);
 
   xpos[0] = 50; ypos [0] = 100;
@@ -86,67 +124,133 @@ void main()
   spr_set_pos(&monk, xpos[0], ypos[0]);
   spr_show(&monk);
 
-  sys_irq_init();
-
+  dir = -1; ctr = 0;
   do {
   	sys_wait_vsync();
   	spr_refresh();
-  	sys_sleep_ms(10);
-  	spr_animate(&monk,1,0);
-  	if (xpos[0]++ < -16) xpos[0] = 255;
+  	spr_animate(&monk,dir,0);
+    xpos[0] += dir; ctr++;
+    if (xpos[0] < -16) xpos[0] = 255;
+    if (xpos[0] > 255) xpos[0] = -16;
+    if (ctr > 300) {
+      ctr = 0;
+      dir *= -1;
+    }
   	spr_set_pos(&monk, xpos[0], ypos[0]);
   	spr_update(&monk);
   } while (sys_get_key(8) & 1);
+}
+
+/**
+ *
+ */
+void test_32x16_1layers() {
+  int8_t dir;
+  uint16_t ctr;
+  spr_init();
+  SPR_DEFINE_PATTERN_SET(PATRN_BAT, SPR_SIZE_32x16, 1, 2, two_states, darkbat);
+  spr_valloc_pattern_set(PATRN_BAT);
+
+  xpos[0] = 20; ypos [0] = 100;
+  spr_init_sprite(&bat, PATRN_BAT);
+  spr_set_pos(&bat, xpos[0], ypos[0]);
+  spr_show(&bat);
+
+  dir = -1; ctr = 0;
+  do {
+  	sys_wait_vsync();
+  	spr_refresh();
+  	spr_animate(&bat,dir,0);
+    xpos[0] += dir; ctr++;
+    if (xpos[0] < -32) xpos[0] = 255;
+    if (xpos[0] > 255) xpos[0] = -32;
+    if (ctr > 300) {
+      ctr = 0;
+      dir *= -1;
+    }
+  	spr_set_pos(&bat, xpos[0], ypos[0]);
+  	spr_update(&bat);
+  } while (sys_get_key(8) & 1);
+}
+
+/**
+ *
+ */
+void test_32x32_1layer() {
+  int8_t dir;
+  uint16_t ctr;
 
   spr_init();
+  SPR_DEFINE_PATTERN_SET(PATRN_DEATH, SPR_SIZE_32x32, 1, 2, two_states2, death);
+  spr_valloc_pattern_set(PATRN_DEATH);
 
+  xpos[0] = 50; ypos [0] = 100;
+  spr_init_sprite(&death_spr, PATRN_DEATH);
+  spr_set_pos(&death_spr, xpos[0], ypos[0]);
+  spr_show(&death_spr);
 
+  dir = -1; ctr = 0;
+  do {
+  	sys_wait_vsync();
+  	spr_refresh();
+  	spr_animate(&death_spr,dir,0);
+    xpos[0] += dir; ctr++;
+    if (xpos[0] < -32) xpos[0] = 255;
+    if (xpos[0] > 255) xpos[0] = -32;
+    if (ctr > 300) {
+      ctr = 0;
+      dir *= -1;
+    }
+  	spr_set_pos(&death_spr, xpos[0], ypos[0]);
+  	spr_update(&death_spr);
+  } while (sys_get_key(8) & 1);
+ }
 
-	/**
-	 * Single layer sprites with animation in two directions
-	 */
+/**
+ * Test 5th sprite interleaving
+ */
+void test_5th_sprite() {
+  int i, dir1, dir2;
+  spr_init();
+  /**
+   * Single layer sprites with animation in two directions
+   */
 
-	SPR_DEFINE_PATTERN_SET(PATRN_BEE, SPR_SIZE_16x16, 1, 2,
-		two_states, bee1);
-	SPR_DEFINE_PATTERN_SET(PATRN_RAT, SPR_SIZE_16x16, 1, 2,
-		two_states, rat);
-	SPR_DEFINE_PATTERN_SET(PATRN_EGG, SPR_SIZE_16x16, 2, 4,
-		four_states, eggerland);
+  SPR_DEFINE_PATTERN_SET(PATRN_BEE, SPR_SIZE_16x16, 1, 2,
+    two_states, bee1);
+  SPR_DEFINE_PATTERN_SET(PATRN_RAT, SPR_SIZE_16x16, 1, 2,
+    two_states, rat);
 
-	spr_valloc_pattern_set(PATRN_BEE);
-	spr_valloc_pattern_set(PATRN_RAT);
-	spr_valloc_pattern_set(PATRN_EGG);
+  spr_valloc_pattern_set(PATRN_BEE);
+  spr_valloc_pattern_set(PATRN_RAT);
 
-	for (i = 0; i< 10; i++) {
-		spr_init_sprite(&bee[i], PATRN_BEE);
-		spr_init_sprite(&rats[i], PATRN_RAT);
-		xpos[i] = i * 20; ypos[i] = i * 20;
-		xpos2[i] = 16 + i * 20; ypos2[i] = 16 + i * 20;
-		spr_set_pos(&bee[i], xpos[i], ypos[i]);
-		spr_set_pos(&rats[i], xpos2[i], ypos2[i]);
-		spr_show(&bee[i]);
-		spr_show(&rats[i]);
-	}
+  for (i = 0; i< 6; i++) {
+    spr_init_sprite(&bee[i], PATRN_BEE);
+    spr_init_sprite(&rats[i], PATRN_RAT);
+    xpos[i] = i * 20; ypos[i] = 70;
+    xpos2[i] = 100 + i * 20; ypos2[i] = 150;
+    spr_set_pos(&bee[i], xpos[i], ypos[i]);
+    spr_set_pos(&rats[i], xpos2[i], ypos2[i]);
+    spr_show(&bee[i]);
+    spr_show(&rats[i]);
+  }
 
-	spr_init_sprite(&eggspr, PATRN_EGG);
-	spr_set_pos(&eggspr, 100, 100);
-	spr_show(&eggspr);
+  dir1 = 1; dir2 = -1;
+  do {
+    sys_wait_vsync();
+    spr_refresh();
+    for (i = 0; i< 6; i++) {
+      spr_animate(&bee[i],1,-1);
+      spr_animate(&rats[i],-1,1);
+      spr_set_pos(&bee[i], xpos[i], ypos[i]);
+      spr_set_pos(&rats[i], xpos2[i], ypos2[i]);
+      ypos[i] += dir1;
+      ypos2[i] += dir2;
+      if (ypos[i] < 0 || ypos[i] > 160) dir1 *= -1;
+      if (ypos2[i] < 0 || ypos2[i] > 160) dir2 *= -1;
+      spr_update(&bee[i]);
+      spr_update(&rats[i]);
+    }
+  } while (sys_get_key(8) & 1);
 
-	do {
-		sys_wait_vsync();
-		spr_refresh();
-		for (i = 0; i< 10; i++) {
-			spr_animate(&bee[i],1,-1);
-			spr_animate(&rats[i],-1,1);
-			spr_set_pos(&bee[i], ++xpos[i], --ypos[i]);
-			spr_set_pos(&rats[i], --xpos2[i], ++ypos2[i]);
-			spr_update(&bee[i]);
-			spr_update(&rats[i]);
-			spr_update(&eggspr);
-		}
-	} while (sys_get_key(8) & 1);
-
-
-	do {
-	} while (sys_get_key(8) & 1);
 }
