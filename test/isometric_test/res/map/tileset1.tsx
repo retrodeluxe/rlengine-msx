c7bbd22d86afa055a2c56036e0d5278775557a50
geijoenr <geijoenr@gmail.com>
2021-03-27 15:50:29 +0100

    isometric experiments

---
+++ test/isometric_test/res/map/tileset1.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="test" tilewidth="32" tileheight="128" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="128" source="wall1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="128" source="wall2.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="128" source="floor1.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="128" source="wallcorner.png"/>
+ </tile>
+</tileset>
